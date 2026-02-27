c9c417a513728a73bea303e574c5d30a1f740444
han-peipei <82298194+han-peipei@users.noreply.github.com>
2026-02-27 13:31:22 +0800

    Update data_main2.py

--- data_3_B_uv_seed_stas2_3_2_1_54641_3d_2_p_tu3/data_main2.py
+++ data_3_B_uv_seed_stas2_3_2_1_54641_3d_2_p_tu3/data_main2.py
@@ -39,7 +39,7 @@ def to_array(x):
 #     raise ValueError(f"Unsupported labels shape {y.shape}")
 
 # ---------------- 配置区 ----------------
-ROOT = "/kaggle/input/datasets/niaosilius/2324-select-droptrain/stations_2324_select_drop(train)"   # 
+ROOT = "/kaggle/input/datasets/niaosilius/stations-2324-train-drop-title/stations_2324_train_drop"   # 
 # ROOT = "/thfs1/home/qx_hyt/hpp/data/station_AI/train_data2"   
 csv_path = "2023.csv"  
 VARS = ("10u", "10v")                                        
